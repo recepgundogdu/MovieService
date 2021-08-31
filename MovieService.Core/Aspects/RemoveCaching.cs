b19202fea0edcf48e8160f2c2b73029c1518adee
Recep Gündoğdu <recep.ua@hotmail.com>
2021-08-31 16:54:37 +0300

    Add files via upload

--- MovieService.Core/Aspects/RemoveCaching.cs
+++ MovieService.Core/Aspects/RemoveCaching.cs
@@ -5,20 +5,22 @@ using System.Runtime.Caching;
 namespace MovieService.Core.Aspects
 {
     [Aspect(Scope.Global)]
-    [Injection(typeof(Caching))]
+    [Injection(typeof(RemoveCaching))]
     public class RemoveCaching : Attribute
     {
         [Advice(Kind.Around, Targets = Target.Method)]
         public object HandleMethod([Argument(Source.Name)] string name, [Argument(Source.Arguments)] object[] arguments, [Argument(Source.Target)] Func<object[], object> method)
         {
+            object result = null;
             ObjectCache cache = MemoryCache.Default;
 
             foreach (var item in cache)
             {
-                cache.Remove(item.Key);
+                if (item.Key.Contains(method?.Target?.ToString()))
+                    cache.Remove(item.Key);
             }
-        
-            return null;
+            result = method(arguments);
+            return result;
         }
     }
 }
